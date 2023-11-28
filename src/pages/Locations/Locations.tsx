602b32505cb0cb3c8c87a18eb9c3e77b6764be9a
Leonardo A. Alvarenga <leosgalvarenga@protonmail.com>
2023-11-28 18:50:17 -0300

    feat: add locations page

---
+++ src/pages/Locations/Locations.tsx
@@ -0,0 +1,12 @@
+import { Navbar, PageWrapper } from '../../components';
+
+function Locations() {
+  return (
+    <>
+      <Navbar />
+      <PageWrapper></PageWrapper>
+    </>
+  );
+}
+
+export default Locations;
