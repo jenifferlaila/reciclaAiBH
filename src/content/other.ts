ece1479c74ba1303ccc25e45947b3c03c7414bab
Leonardo A. Alvarenga <leosgalvarenga@protonmail.com>
2023-11-28 18:13:51 -0300

    feat: finish 404 page

--- src/content/other.ts
+++ src/content/other.ts
@@ -1,3 +1,6 @@
 export default {
   'navbar.title': 'Recicla Aí BH',
+  'notFound.back': 'Voltar para o início',
+  'notFound.title': 'Não encontrado',
+  'notFound.subtitle': 'A página ou item que você procura não existe',
 } as const;
