61dc51c393ece1f5dbbb5bf1169e19eefc7d15c6
Leonardo A. Alvarenga <leosgalvarenga@protonmail.com>
2023-11-28 14:51:01 -0300

    feat: add arrow indicator

--- src/pages/Home/components/TitleCard/TitleCard.style.ts
+++ src/pages/Home/components/TitleCard/TitleCard.style.ts
@@ -1,5 +1,6 @@
+import { ArrowDownwardRounded } from '@mui/icons-material';
 import { Box, Typography } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { keyframes, styled } from '@mui/material/styles';
 
 export const Wrapper = styled(Box)({
   gap: '2rem',
@@ -26,4 +27,19 @@ export const ActionArea = styled(Box)({
   display: 'flex',
   alignItems: 'center',
   flexDirection: 'column',
+});
+
+const arrowAnimation = keyframes`
+  0%, 100% {
+    transform: translateY(-25%);
+    animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
+  }
+  50% {
+    transform: translateY(0);
+    animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+  }
+`;
+
+export const ArrowDown = styled(ArrowDownwardRounded)({
+  animation: `${arrowAnimation} 1s infinite ease`,
 });
--- src/pages/Home/components/TitleCard/TitleCard.tsx
+++ src/pages/Home/components/TitleCard/TitleCard.tsx
@@ -1,8 +1,8 @@
-import { useMediaQuery, useTheme } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useMediaQuery, useTheme } from '@mui/material';
 
 import { useContent } from '../../../../content';
-import { ActionArea, ImageContainer, Title, Wrapper } from './TitleCard.style';
+import { ActionArea, ArrowDown, ImageContainer, Title, Wrapper } from './TitleCard.style';
 import { Button } from '../../../../components';
 
 function TitleCard() {
@@ -14,7 +14,7 @@ function TitleCard() {
   return (
     <Wrapper sx={{ minHeight: { xs: '85vh', md: 'unset' }, width: { xs: '100%', lg: undefined } }}>
       <ImageContainer>
-        <img src="/recycle.svg" alt="Recicle" style={{ maxWidth: 550, minWidth: 300 }} />
+        <img src="/recycle.svg" alt="Recicle" style={{ maxWidth: 500, minWidth: 300 }} />
       </ImageContainer>
 
       <Title variant={isLarge ? 'h3' : 'h4'} sx={{ justifyContent: isLarge ? undefined : 'space-evenly' }}>
@@ -33,6 +33,10 @@ function TitleCard() {
           </Button>
         </Link>
       </ActionArea>
+
+      <Link to="/#noticias">
+        <ArrowDown fontSize="large" />
+      </Link>
     </Wrapper>
   );
 }
