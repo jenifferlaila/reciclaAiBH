18b6126a6b4bf8b48f693607fdc28f33d2ccc9de
Jeniffer Laila <jenifferlaila@protonmail.com>
2023-11-29 16:48:18 -0300

    feat: add gov links about garbage retrieval

--- src/content/about.ts
+++ src/content/about.ts
@@ -1,7 +1,14 @@
 export default {
-    'about.title': 'Você Sabia?',
-    'about.subTitle': 'Que cada brasileiro gera em média 379,2 Kg de lixo por ano e que deste nem 10% é destinado a reciclagem?',
-    'about.font': '*Fonte: Panorama dos Resíduos Sólidos no Brasil 2020, estudo realizado pela Associação Brasileira das Empresas de Limpeza Pública e Resíduos Especiais',
-    'about.info': 'Para mudar esse panorama, antes de levar nos pontos de coleta é preciso entender quais materiais se enquadram como resíduos sólidos recicláveis.', 
-    'about.example': 'São esses:'
-} as const
+  'about.title': 'Você Sabia?',
+  'about.subTitle':
+    'Que cada brasileiro gera em média 379,2 Kg de lixo por ano e que deste nem 10% é destinado a reciclagem?',
+  'about.font':
+    '*Fonte: Panorama dos Resíduos Sólidos no Brasil 2020, estudo realizado pela Associação Brasileira das Empresas de Limpeza Pública e Resíduos Especiais',
+  'about.info':
+    'Para mudar esse panorama, antes de levar nos pontos de coleta é preciso entender quais materiais se enquadram como resíduos sólidos recicláveis.',
+  'about.example': 'São esses:',
+  'about.retrieval.label':
+    'Você pode conferir os bairros que recebem a coleta seletiva porta a porta acessando o portal da Prefeitura de Belo Horizonte',
+  'about.retrieval.door': 'Pontos de coleta porta a porta',
+  'about.retrieval.schedule': 'Horários de coleta',
+} as const;
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,69 +1,79 @@
 import { Typography, useMediaQuery, useTheme } from '@mui/material';
-import { Navbar, PageWrapper } from '../../components';
-import { Image, DivSubTitle} from './About.styles';
+import { Button, Navbar, PageWrapper } from '../../components';
+import { Image, DivSubTitle, LinkArea } from './About.styles';
 import { useContent } from '../../content';
+import { MeetingRoomRounded, QueryBuilderRounded } from '@mui/icons-material';
 
 function About() {
-  const {breakpoints} = useTheme();
-  const islarge = useMediaQuery(breakpoints.up('md'));
-  const {t} = useContent()
+  const { breakpoints } = useTheme();
+  const isLarge = useMediaQuery(breakpoints.up('md'));
+  const { t } = useContent();
   return (
     <>
       <Navbar />
       <PageWrapper>
-        <Image
-        style={{backgroundImage: 'url(/rectangle.png)'}}
-        />
+        <Image style={{ backgroundImage: 'url(/rectangle.png)' }} />
         <Typography
-          variant={islarge ? 'h3' : 'h5'}
+          variant={isLarge ? 'h3' : 'h5'}
           sx={{
             marginTop: '10rem',
-            textAlign: 'center'
+            textAlign: 'center',
           }}
         >
-        {t('about.title')}
+          {t('about.title')}
         </Typography>
         <DivSubTitle
-          sx={{ 
-          marginTop: islarge? '-4rem' : undefined,
-          marginBottom: islarge? '-2rem': undefined}}
+          sx={{
+            marginTop: isLarge ? '-4rem' : undefined,
+            marginBottom: isLarge ? '-2rem' : undefined,
+          }}
         >
-          <img 
-            src= '/lixo-pilha.svg'
-          >
-          </img>
-          <Typography
-            variant={islarge ? 'h5' : 'body1'}
-            >
-            {t('about.subTitle')}
-          </Typography>
+          <img src="/lixo-pilha.svg" alt="Pilha de lixo" />
+          <Typography variant={isLarge ? 'h5' : 'body1'}>{t('about.subTitle')}</Typography>
         </DivSubTitle>
-        <div style={{
-          padding: '0 4rem',
-          width: '100%'
-          }}>
-          <Typography
-            variant={islarge ? 'h6' : 'body2'}
-            >
-            {t('about.info')}
-          </Typography>
-          <Typography
-            variant={islarge ? 'h6' : 'body2'}
-            style={{fontWeight: '900'}}
-            >
+        <div
+          style={{
+            padding: '0 4rem',
+            width: '100%',
+          }}
+        >
+          <Typography variant={isLarge ? 'h6' : 'body2'}>{t('about.info')}</Typography>
+
+          <Typography variant={isLarge ? 'h6' : 'body2'} style={{ fontWeight: '900' }}>
             {t('about.example')}
           </Typography>
-          <img 
-              src= '/descarte.svg'
-              width= {islarge? '40%' : '90%'}
-            >
-            </img>
-          <Typography
-            variant="body2"
-            style={{fontSize: islarge? '12px' : '6px'}}
-          >
+
+          <img src="/descarte.svg" width={isLarge ? '40%' : '90%'} alt="Descarte" />
+
+          <Typography variant="body2" style={{ fontSize: isLarge ? '12px' : '6px' }}>
             {t('about.font')}
           </Typography>
+
+          <LinkArea>
+            <Typography variant={isLarge ? 'h6' : 'body2'}>{t('about.retrieval.label')}</Typography>
+
+            <div className="action-area">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://prefeitura.pbh.gov.br/slu/informacoes/servicos/porta-a-porta#BARREIRO"
+              >
+                <Button variant="contained" sx={{ boxShadow: 8 }} startIcon={<MeetingRoomRounded />}>
+                  {t('about.retrieval.door')}
+                </Button>
+              </a>
+
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://prefeitura.pbh.gov.br/slu/informacoes/servicos/coleta-domiciliar"
+              >
+                <Button variant="contained" sx={{ boxShadow: 8, width: '100%' }} startIcon={<QueryBuilderRounded />}>
+                  {t('about.retrieval.schedule')}
+                </Button>
+              </a>
+            </div>
+          </LinkArea>
         </div>
       </PageWrapper>
     </>
